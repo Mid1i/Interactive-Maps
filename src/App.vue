1007f87b72c234f149c8f69439108e3db808f6bb
Mid1i <michac.tchernov@yandex.ru>
2024-11-01 21:38:05 +0300

    feat: Добавлена отрисовка карты с базовыми настройками управления

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,69 @@
 <script setup lang="ts">
-	import { RouterView } from "vue-router";
+	import { shallowRef, ref } from "vue";
+	import type { DomEventHandler, YMap, YMapLocationRequest } from '@yandex/ymaps3-types';
+	import { 
+		YandexMap,
+		YandexMapDefaultSchemeLayer, 
+		YandexMapDefaultFeaturesLayer, 
+		YandexMapListener,
+		YandexMapControls,
+		YandexMapGeolocationControl, 
+		YandexMapZoomControl
+	} from "vue-yandex-maps";
+
+
+	const map = shallowRef<null | YMap>(null);
+
+	const LOCATION = ref<YMapLocationRequest>({
+		center: [0, 0],
+		zoom: 15
+	});
+
+	const success = ({ coords }: { coords: GeolocationCoordinates }) => {
+		const { latitude, longitude } = coords;
+
+		LOCATION.value = {
+			center: [longitude, latitude],
+			zoom: 15
+		}
+	}
+
+	const error = ({ message }: { message: string }) => {
+		console.log(message);
+	}
+
+	navigator.geolocation.getCurrentPosition(success, error, { enableHighAccuracy: true });
+
+	const logMapClick: DomEventHandler = (object, event) => console.log(object, event);
 </script>
 
 
 <template>
-	<RouterView/>
+	<yandex-map 
+		v-model="map" 
+		:settings="{
+			location: {
+				...LOCATION,
+				duration: 2500
+			},
+			showScaleInCopyrights: true
+		}" 
+		real-settings-location
+	>
+		<YandexMapDefaultSchemeLayer/>
+		<YandexMapDefaultFeaturesLayer/>
+		<YandexMapListener :settings="{ onClick: logMapClick }"/>
+		<yandex-map-controls :settings="{ position: 'right', orientation: 'vertical' }">
+			<YandexMapZoomControl/>
+			<YandexMapGeolocationControl/>
+    </yandex-map-controls>
+	</yandex-map>
 </template>
 
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+	.map {
+		height: 100%;
+		width: 100%;
+	}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,16 @@ import { createApp } from "vue";
 import "@/assets/styles/base.scss";
 import { router } from "@/router";
 import App from "./App.vue";
+import { createYmaps } from "vue-yandex-maps";
+
+const maps = createYmaps({
+	apikey: import.meta.env.VITE_YANDEX_APIKEY
+});
 
 
 const Vue = createApp(App);
 
 Vue.use(router);
+Vue.use(maps)
 
 Vue.mount('#app');
