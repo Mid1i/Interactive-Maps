84e9b817a4c90d43ac25e7d724e042e71f46c2d5
Mid1i <michac.tchernov@yandex.ru>
2024-11-01 21:25:39 +0300

    fix: Небольшие изменения

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,16 @@ import { createApp } from "vue";
 import "@/assets/styles/base.scss";
 import { router } from "@/router";
 import App from "./App.vue";
+import { createYmaps } from "vue-yandex-maps";
+
+const maps = createYmaps({
+	apikey: "f6c3579b-1f4a-4d23-8eb6-cc071226d4c0" 
+});
 
 
 const Vue = createApp(App);
 
 Vue.use(router);
+Vue.use(maps)
 
 Vue.mount('#app');
